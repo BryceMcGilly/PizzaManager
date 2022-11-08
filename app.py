2e5d27952dd1112383a9888eef8e7776bcc0c640
BryceMcGilly <117692175+BryceMcGilly@users.noreply.github.com>
2022-11-07 20:48:23 -0700

    Changed database URI for SQLAlchemy to work with Heroku Postgres

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+uri = os.environ.get('DATABASE_URL')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 db = SQLAlchemy(app)
