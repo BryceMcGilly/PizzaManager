5910f7b9ae6947282ca8ec7832a5223e37daaa94
BryceMcGilly <117692175+BryceMcGilly@users.noreply.github.com>
2022-12-09 14:11:41 -0700

    Debug Heroku production environment.

--- pizzamanager/__init__.py
+++ pizzamanager/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
 
 from .routes import main
 from .db import db
 
-def create_app(config_file='settings.py'):
+def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile(config_file)
+
+    load_dotenv()
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('HEROKU_POSTGRESQL_OLIVE_URL')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
     db.init_app(app)
     app.register_blueprint(main)
     with app.app_context():
