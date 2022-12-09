f53a6227a978eb37513950c7fdc3d61149cf2b7c
BryceMcGilly <117692175+BryceMcGilly@users.noreply.github.com>
2022-12-09 12:37:09 -0700

    Use load_dotenv for production.

--- pizzamanager/__init__.py
+++ pizzamanager/__init__.py
@@ -1,20 +1,11 @@
 from flask import Flask
-import os
 
 from .routes import main
 from .db import db
 
-def create_app():
+def create_app(config_file='settings.py'):
     app = Flask(__name__)
-
-    uri = os.environ.get('HEROKU_POSTGRESQL_OLIVE_URL')
-    #Make URI compatible with Heroku
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = uri
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-
+    app.config.from_pyfile(config_file)
     db.init_app(app)
     app.register_blueprint(main)
     with app.app_context():
