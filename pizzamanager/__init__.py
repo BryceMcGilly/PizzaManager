a27f5d708d4530dbd59ed49e227fae0aed9de3ed
BryceMcGilly <117692175+BryceMcGilly@users.noreply.github.com>
2022-12-09 22:25:06 -0700

    Remove prints.

--- pizzamanager/__init__.py
+++ pizzamanager/__init__.py
@@ -7,15 +7,13 @@ from .db import db
 def create_app():
     app = Flask(__name__)
 
+    #Make Database URL compatible between SQLAlchemy and Heroku
     uri = os.environ.get('DATABASE_URL')
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-
     app.config['SQLALCHEMY_DATABASE_URI'] = uri
-    print("DB: " + app.config.get('SQLALCHEMY_DATABASE_URI'))
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    print("Secret: " + app.config.get('SECRET_KEY'))
-
+    
     db.init_app(app)
     app.register_blueprint(main)
     with app.app_context():
